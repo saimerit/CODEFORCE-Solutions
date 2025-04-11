a6d3ddd600a1a1eef0f0612fac3faad1911e6061
Sai Ardhendu <saiardhendu10@gmail.com>
2025-04-11 10:58:06 +0530

    solution to 1877A

--- 1877A.cpp
+++ 1877A.cpp
@@ -1,1 +1,18 @@
-#include <ios>
+#include <iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        int ans = 0, k;
+        for (int i = 0; i < n-1; i++){
+            cin >> k;
+            ans+=k;
+        }
+        cout << -ans << endl;
+    }
+    return 0;
+}
